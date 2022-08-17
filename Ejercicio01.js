8f980545500bac765a1fce7f1643d4cdd5be4715
franromeroe <franromeroeze@gmail.com>
2022-08-17 11:49:58 -0300

    Ejerciciosjs terminados

---
+++ Ejercicio01.js
@@ -0,0 +1,9 @@
+/* 
+-Comprendiendo las variables no inicializadas
+
+ejercicio
+Inicializa las tres variables a, b, y c con 5, 10 y "I am a" respectivamente para que no sean undefined.
+*/
+var a = 5;
+var b = 10;
+var c = "I am a";
---
+++ Ejercicio02.js
@@ -0,0 +1,15 @@
+/* 8. Comprendiendo la sensibilidad de mayúsculas en las variables
+
+ejercicio
+Modifica las declaraciones y asignaciones existentes para que sus nombres usen camelCase.
+No crees ninguna variable nueva.
+*/
+// Declaración de variables
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Asignación de variables
+studlyCapVar = 10;
+properCamelCase= "A String";
+titleCaseOver = 9000;
