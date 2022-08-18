bd387adb567debbb36801adddab1a12e8ef2047a
franromeroe <franromeroeze@gmail.com>
2022-08-18 12:08:55 -0300

    Ejercicios Terminados freecodecamp

---
+++ Ejercicio13.js
@@ -0,0 +1,8 @@
+/*
+Multiplica dos números con JavaScript
+
+Cambia el 0 para que el producto sea igual a 80.
+
+*/
+
+const product = 8 * 10;
---
+++ Ejercicio15.js
@@ -0,0 +1,11 @@
+/*
+Incrementa un número con JavaScript
+
+Cambia el código para usar el operador ++ en myVar.
+
+*/
+
+let myVar = 87;
+
+
+myVar++;
