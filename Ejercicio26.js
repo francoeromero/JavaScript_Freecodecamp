2b75b28b6d74786c99f3b965ec8bda67588e411b
franromeroe <franromeroeze@gmail.com>
2022-08-20 20:55:55 -0300

    ejercicios terminados

---
+++ Ejercicio26.js
@@ -0,0 +1,8 @@
+/*
+Cita cadenas con comillas simples
+
+
+Cambia la cadena proporcionada a una cadena con comillas simples al principio y al final y sin caracteres de escape.
+*/
+
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
