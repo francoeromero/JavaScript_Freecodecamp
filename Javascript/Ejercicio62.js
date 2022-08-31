1835d7f63352448297c83f0195448ca09b6f7a36
franromeroe <franromeroeze@gmail.com>
2022-08-31 12:50:13 -0300

    ejercicios terminados

---
+++ Javascript/Ejercicio62.js
@@ -0,0 +1,9 @@
+// Practica comparando diferentes valores
+function compareEquality(a, b) {
+  if (a === b) { // Cambia esta línea
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+compareEquality(10, 10);
