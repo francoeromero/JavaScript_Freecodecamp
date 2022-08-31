b700aa946e84778e1f9599d5ca690b9f714468f3
franromeroe <franromeroeze@gmail.com>
2022-08-31 12:57:54 -0300

    ejercicios terminados

---
+++ Javascript/Ejercicio63.js
@@ -0,0 +1,9 @@
+// Comparación con el operador de desigualdad
+function testNotEqual(val) {
+  if (val != 99) { // Cambia esta línea
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testNotEqual(10);
