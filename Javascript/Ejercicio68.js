71ea1f0fd6c984c38e9cd857c709e1f410229157
franromeroe <franromeroeze@gmail.com>
2022-09-01 10:55:40 -0300

    ejercicios terminados

---
+++ Javascript/Ejercicio68.js
@@ -0,0 +1,14 @@
+//Comparación con el operador "menor o igual que"
+function testLessOrEqual(val) {
+  if (val <= 12) {  // Cambia esta línea
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val <= 24) {  // Cambia esta línea
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+testLessOrEqual(10);
---
+++ Javascript/Ejercicio69.js
@@ -0,0 +1,12 @@
+//Comparaciones con el operador lógico "and"
+function testLogicalAnd(val) {
+  
+  if (val <= 50 && val >= 25) {
+   
+      return "Yes";
+    }
+
+      return "No";
+}
+
+testLogicalAnd(10);
