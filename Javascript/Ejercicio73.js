adc0979061294e5ceeb2ed55c408265804a180d1
franromeroe <franromeroeze@gmail.com>
2022-09-02 13:54:49 -0300

    ejercicios terminados

---
+++ Javascript/Ejercicio73.js
@@ -0,0 +1,12 @@
+//Orden lógico de las sentencias "if else"
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);
