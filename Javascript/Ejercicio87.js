01e912ca948213f300e4270d8840bfb95f3460c4
franromeroe <franromeroeze@gmail.com>
2023-01-03 10:03:34 -0300

    cambiar el nombre a una propiedad del objeto

---
+++ Javascript/Ejercicio87.js
@@ -0,0 +1,21 @@
+//Actualizando las propiedades de un objeto
+
+// Configuración
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  
+  myDog.name = "Happy Coder";
+
+  
+
+  /*
+  
+Para cambiar el valor de una propiedad del objeto es asi: 
+
+nombreObjeto.propiedadObjeto = "valorObjeto";
+  
+  */
