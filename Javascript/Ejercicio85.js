2dfb6334a10498c8f086b31b57493d81ab607949
franromeroe <franromeroeze@gmail.com>
2023-01-03 09:41:08 -0300

    acceder a los objetos con corchetes

---
+++ Javascript/Ejercicio85.js
@@ -0,0 +1,20 @@
+//Accede a propiedades de objetos con notación de corchete
+// Configuración
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  // Cambia solo el código debajo de esta línea
+  const entreeValue = testObj["an entree"];   // Cambia esta línea
+  const drinkValue = testObj["the drink"];    // Cambia esta línea
+
+  /*
+  Esta es otra manera de seleccionar la propiedad de un objeto en JS con notacion corchetes 
+
+  1. Creamos la variable constante 
+  2. llamamos al nombre del objeto que queremos seleccionar su propiedad
+  3. Luego abrimos [] y colocamos las propiedades dentro del objeto seleccionado
+  
+  */
