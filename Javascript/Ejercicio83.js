a8707cfbc784959a134d3ee342b726c98e59fddd
franromeroe <franromeroeze@gmail.com>
2023-01-03 09:25:24 -0300

    estructura de un objeto JS

---
+++ Javascript/Ejercicio83.js
@@ -0,0 +1,20 @@
+// Construye objetos en JavaScript
+
+const myDog = {
+  name: "Firulais",
+  legs: 4,
+  tails: 1,
+  friends: ["Cats", "Humans"]
+};
+
+/*
+estructura de un objeto 
+
+const nombredelObjeto = {
+    propiedad : valor,
+    propiedad : valor,
+    propiedad : valor
+};
+
+
+*/
