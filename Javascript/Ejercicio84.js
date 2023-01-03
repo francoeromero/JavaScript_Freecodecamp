9461db056e9f2b3a836e713e56f3f10d95213412
franromeroe <franromeroeze@gmail.com>
2023-01-03 09:28:18 -0300

    acceder ala propiedad de un objeto

---
+++ Javascript/Ejercicio84.js
@@ -0,0 +1,26 @@
+// ▬Accede a propiedades de objetos con notación de puntos
+
+
+// Configuración
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Cambia solo el código debajo de esta línea
+const hatValue = testObj.hat;      // Cambia esta línea
+const shirtValue = testObj.shirt;    // Cambia esta línea
+
+/*
+
+para acceder ala propiedad de un objeto es asi :
+
+ 	nombre de objeto + . + propiedad 
+
+    
+*/
+
+
+
+
