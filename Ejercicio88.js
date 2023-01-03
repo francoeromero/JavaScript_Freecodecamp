7bafc48d15def6e0e7cde91346e3fcf06268babf
franromeroe <franromeroeze@gmail.com>
2023-01-03 10:06:51 -0300

    añadir nueva propiedad al objeto

---
+++ Ejercicio88.js
@@ -0,0 +1,18 @@
+//Añade nuevas propiedades a un objeto de JavaScript
+
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+
+  myDog["bark"] = "woof";
+  
+  /*
+  Para añadir propiedad y valor a un objeto es asi:
+
+  nombreObjeto[nuevapropiedad] = valor;
+  
+  
+  */
