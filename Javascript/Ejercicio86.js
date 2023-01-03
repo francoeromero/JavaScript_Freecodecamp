5c28b0dd797dd7b79068b5d66861ccaa806ecc10
franromeroe <franromeroeze@gmail.com>
2023-01-03 09:57:24 -0300

    acceder a variables para llamar a una propiedad y su valor

---
+++ Javascript/Ejercicio86.js
@@ -0,0 +1,28 @@
+//Accede a propiedades de objetos con variables
+
+// Configuración
+const testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+  };
+  
+ // Cambia solo el código debajo de esta línea
+const playerNumber = 16;  // Cambia esta línea
+const player = testObj[playerNumber];   // Cambia esta línea
+
+  /*
+
+1-  Asignamos la propiedad a una variable const
+
+        variableConstante1 =  nombrePropiedadObjeto; 
+
+2- luego asignamos el nombre del objeto con la primera variable a la segunda variable const
+
+         variableConstante2 =  nombreObjeto [variableConstante1];
+
+3- Por ultimo llamamos ala variable 2, y lo que va a salir en consola es el valor de la propiedad
+
+        console.log(variableConstante2);
+  
+  */
