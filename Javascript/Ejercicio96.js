99572e354b74faa11708c224bdacef2653b6fe06
franromeroe <franromeroeze@gmail.com>
2023-01-11 13:17:10 -0300

    while y arreglo

--- Javascript/Ejercicio96.js
+++ Javascript/Ejercicio96.js
@@ -1,20 +1,43 @@
 //Itera con el bucle "while" de JavaScript
 // Agrega los números de 5 a 0 (inclusivo) en orden descendente a myArray usando un bucle while.
 
-// 1 -  creamos una variable de arreglo vacio
-const myArray = [];  
 
-// 2 -  inicializamos con 0 
-let i = 0; 
 
-// 3 -  la cantidad de vueltas del bucle
-while(i < 4){ 
-// 4 -  empuja los valores a un arreglo
-    myArray.push(i); 
-// 5 -  el contador se suma 1, para seguir el bucle 0, 1, 2, 3, 4 o tambien se pone i++ 
-    i = i + 1;
+const myArray = []; 
+let i = 5; //6 vueltas, [0, 1, 2, 3, 4, 5]
+while(i >= 0){ 
+    myArray.push(i); //pusheo al arreglo los valores
+    i = i - 1; // i--
 }
 
-// 6- Por ultimo mostramos el resultado en la consola
+console.log(myArray); //mostramos en consola
 
-console.log(myArray);
+
+
+
+
+
+// // 1 -  creamos una variable de arreglo vacio
+
+// const myArray = [];  
+
+// // 2 -  inicializamos con 0 
+
+// let i = 0; 
+
+// // 3 -  la cantidad de vueltas del bucle
+
+// while(i < 4){ 
+
+// // 4 -  empuja los valores a un arreglo
+
+//     myArray.push(i); 
+
+// // 5 -  el contador se suma 1, para seguir el bucle 0, 1, 2, 3, 4 o tambien se pone i++ 
+
+//     i = i + 1;
+// }
+
+// // 6- Por ultimo mostramos el resultado en la consola
+
+// console.log(myArray);
