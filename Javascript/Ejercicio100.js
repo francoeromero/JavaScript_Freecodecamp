6c3c63ab9f0234ab9e9b4ca5e262a18b3578d0c1
franromeroe <franromeroeze@gmail.com>
2023-01-12 19:10:13 -0300

    usar length para la cantidad de vueltas del bucle

--- Javascript/Ejercicio100.js
+++ Javascript/Ejercicio100.js
@@ -1,7 +1,34 @@
 //Itera a través de un arreglo con un bucle "for"
 
+
+//EJEMPLO
+let ejemplo = [9, 10, 11, 12];
+let total = 0;
+
+for (let i = 0; i < ejemplo.length; i++){
+    total += ejemplo[i];
+}
+console.log(total); // esto daria 42, porque esta sumando a 9,10,11,12
+
+// ejemplo.length ---> traducido es como decir "la cantidad de valores en el ejemplo seria 4"
+
+
+
+
 const myArr = [2, 3, 4, 5, 6];
 
-for (let i = 0; i < myArr.lenght; i++){
-    console.log(arr[i]);
+for (let i = 0; i < myArr.lenght; i++){ // la cantidad de vueltas del bucle lo define la cantidad de valores del arreglo en este caso el arreglo es [2, 3, 4, 5, 6]
+    total += myArr[i]; // agrego cada valor i por cada vuelta
 }
+
+
+
+// Setup
+var myArr = [ 2, 3, 4, 5, 6];
+var total = 0;
+
+for (var i = 0; i < myArr.length; i++) {
+  total += myArr[i];
+}
+
+console.log(total);
