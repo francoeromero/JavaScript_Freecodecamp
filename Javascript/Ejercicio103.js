82b482046563f1efd9a45f3994bc6f9551b02c15
franromeroe <franromeroeze@gmail.com>
2023-01-12 22:15:11 -0300

    funcion recursiva

--- Javascript/Ejercicio103.js
+++ Javascript/Ejercicio103.js
@@ -15,9 +15,9 @@ function funcionRecursiva(parametro) {
 
 function sum(arr, n) {
   if (n <= 0) { // condición de parada, se detiene la recursión si n es menor o igual a 0
-      return 0;
+      return 0; //si se usa return 1, entonces se estaria devolviendo un valor fijo y no la suma de los elementos del arreglo.
   } else {
       return arr[n - 1] + sum(arr, n - 1); // se retorna el valor en el indice n-1 del arreglo mas la llamada recursiva a la función sum con los parametros arr y n-1
   }
 }
-console.log(sum([1, 2, 3, 4, 5], 3)); // 6
+console.log(sum([1, 2, 3, 4, 5], 3)); // me da 6
