467209cdb278108b2f07df71ecf1c016a4e71205
franromeroe <franromeroeze@gmail.com>
2023-01-13 01:09:53 -0300

    repaso while

---
+++ apuntesJS/While/while.js
@@ -0,0 +1,17 @@
+
+//bucle
+let numero = 0; //iniciamos con cero
+
+while(numero < 6){
+    numero++; 
+    document.write(numero + "<br> <hr> <p>Puedo escribir html</p>");
+}//me muestra 123456 
+
+
+let num = 7;
+
+do {
+    num--;
+    document.write(num + "<p>El do me deja ejecutar al menos 1 vez</p>")
+
+}while(num > 1); //me muestra 654321
