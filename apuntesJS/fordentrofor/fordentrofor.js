a2ca183caeee49669412b61642cbea7df60aba00
franromeroe <franromeroeze@gmail.com>
2023-01-13 12:56:35 -0300

    for avanzado

---
+++ apuntesJS/fordentrofor/fordentrofor.js
@@ -0,0 +1,13 @@
+let array1 = ["maria", "josefa", "roberta"];
+let array2 = ["pedro", "marcelo", array1];
+
+for (let array in array2){
+    if(array == 2){
+        for(let array of array1){
+            document.write(array + "<br>");
+        }
+        else {
+            document.write(array2[array] + "<br>");
+        }
+    }
+}
