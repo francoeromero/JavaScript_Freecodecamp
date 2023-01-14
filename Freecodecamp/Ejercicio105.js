ff753997cde4d0fcd0ad91da502da085391846f1
franromeroe <franromeroeze@gmail.com>
2023-01-14 01:05:50 -0300

    numero random

---
+++ Freecodecamp/Ejercicio105.js
@@ -0,0 +1,30 @@
+//Genera fracciones aleatorias con JavaScript
+
+function randomFraction() {
+
+  return Math.random();  
+}
+
+console.log(randomFraction()); // te devuelve un numero del 0 al 1 random
+
+//EJEMPLOS 
+
+/*
+
+function randomNumber() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+console.log(randomNumber()); // puede imprimir un número entre 1 y 10
+
+
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+console.log(randomNumber(5, 15)); // puede imprimir un número entre 5 y 15
+
+
+
+
+*/
