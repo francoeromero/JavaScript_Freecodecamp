f914dcb79184ad709b40fb6a6838312cac1f0217
franromeroe <franromeroeze@gmail.com>
2023-01-14 02:31:30 -0300

    countdown

--- Freecodecamp/Ejercicio112.js
+++ Freecodecamp/Ejercicio112.js
@@ -4,11 +4,16 @@
 //EJEMPLO 
 
 function countup(n) {
+  // Si n es menor a 1, se devuelve un arreglo vacío
   if (n < 1) {
     return [];
-  } else {
+  } 
+  // Llama recursivamente a la función countup con el argumento n - 1
+  else {
     const countArray = countup(n - 1);
+   // Añade el valor de n al final del arreglo countArray
     countArray.push(n); // PUSHEA
+  // Devuelve el arreglo countArray
     return countArray;
   }
 }
@@ -18,12 +23,16 @@ console.log(countup(5)); //MUESTRA [1, 2, 3, 4, 5]
 //EJERCICIO
 
 function countdown(n) {
+  // Si n es menor a 1, se devuelve un arreglo vacío
   if (n < 1) {
       return [];
   }
+  // Añade el valor de n al principio del arreglo countArray
   else {
       const countArray = countdown(n - 1);
+      // Añade el valor de n al principio del arreglo countArray
       countArray.unshift(n); // UNSHIFT 
+       // Devuelve el arreglo countArray
       return countArray;
   }
 }
