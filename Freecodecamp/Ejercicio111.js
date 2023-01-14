d007750c7b9e633ad5cf150d0b98fc12a1d194f4
franromeroe <franromeroeze@gmail.com>
2023-01-14 02:13:08 -0300

    if else if y else en una sola linea

---
+++ Freecodecamp/Ejercicio111.js
@@ -0,0 +1,12 @@
+//Usa múltiples operadores condicionales (ternarios)
+
+function checkSign(num) {
+  
+  return (num > 0) ? "positive" : (num < 0) ? "negative" : "zero";
+//          if                   else if                  else
+ }
+ 
+ checkSign(10);
+
+
+ 
