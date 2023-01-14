87cf6ee55c7535888f01daebce0d9a9fb64c22f2
franromeroe <franromeroeze@gmail.com>
2023-01-14 01:39:46 -0300

    parseInt

---
+++ Freecodecamp/Ejercicio108.js
@@ -0,0 +1,7 @@
+//Utiliza la función "parseInt"
+
+function convertToInteger(str) {
+  return parseInt(str); // importante el RETURN, lo devuelve y ponemos parseInt para que lo convierta de string a number
+}
+
+console.log(convertToInteger("56"));
