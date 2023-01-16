bc609307e38ad89de3fd6b69319d6cb3d70eb6d0
franromeroe <franromeroeze@gmail.com>
2023-01-16 16:44:10 -0300

    funcion flecha

---
+++ Freecodecamp/Ejercicio117.js
@@ -0,0 +1,6 @@
+
+// Usa funciones flecha para escribir funciones anónimas de manera breve
+
+const magic = () => { // le cambia una variable constante y aplico la otra forma de declarar una funcion que es la flecha
+  return new Date();
+};
