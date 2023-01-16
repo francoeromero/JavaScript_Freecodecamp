b780e1d8680f29f775527f5e7127010aa6144b08
franromeroe <franromeroeze@gmail.com>
2023-01-16 16:49:49 -0300

    con parametros

---
+++ Freecodecamp/Ejercicio118.js
@@ -0,0 +1,26 @@
+
+// Escribe funciones flecha con parámetros
+
+const myConcat = (arr1, arr2) => { // aplique la funcion flecha con parametros
+    return arr1.concat(arr2);
+  };
+  
+  console.log(myConcat([1, 2], [3, 4, 5]));
+
+
+  //esquema 
+/*
+
+ESQUEMA SIN PARAMETROS
+
+  const variable = () => {
+
+  }
+
+ESQUEMA CON PARAMETROS
+
+  const variable = (parametro1, parametro2) => {
+
+  }
+
+  */
