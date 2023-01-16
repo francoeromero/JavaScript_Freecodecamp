fb98beb304e918ef2a2a79b8c7a25e2d5cb7366f
franromeroe <franromeroeze@gmail.com>
2023-01-16 17:32:29 -0300

    operador de propagacion

---
+++ Freecodecamp/Ejercicio121.js
@@ -0,0 +1,9 @@
+
+// Utiliza el operador de propagación para evaluar los arreglos en el lugar
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Cambie "...arr1" que es el operador de propagacion para copiar todo el contenido de arr1
+
+console.log(arr2);
