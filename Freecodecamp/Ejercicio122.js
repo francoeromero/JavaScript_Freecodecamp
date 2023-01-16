54ddfae51c620765fc82a5ad38a0960c5032aaa3
franromeroe <franromeroeze@gmail.com>
2023-01-16 17:46:30 -0300

    sintaxis desestructuracion

---
+++ Freecodecamp/Ejercicio122.js
@@ -0,0 +1,23 @@
+
+// Usa sintaxis de desestructuración para extraer valores de objetos
+
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  //uso la sintaxix de desestructuracion
+
+  const {today, tomorrow} = HIGH_TEMPERATURES;
+  
+  
+  /*
+  ASI ESTABA ANTES, QUE FUE REEMPLAZADO
+  
+  const today = HIGH_TEMPERATURES.today;
+  const tomorrow = HIGH_TEMPERATURES.tomorrow;
+  
+  */
+  
+  
